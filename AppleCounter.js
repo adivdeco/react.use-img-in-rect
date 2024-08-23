4ac144d85320bfbe268109a5fcafb0e79e211e00
adivdeco <165708861+adivdeco@users.noreply.github.com>
2024-08-23 22:56:14 +0530

    ep11
    add all events or style....

--- AppleCounter.js
+++ AppleCounter.js
@@ -1,22 +1,70 @@
 import Button from './Button'
-
 import LeftArrow from "../assets/image/leftarrow.png";
 import RightArrow from "../assets/image/rightarrow.png";
 import AppleBasket from './AppleBasket';
-
 import "./applecounter.css"
 
+import { createRoot } from 'react-dom/client';
+// Ensure the case and path are correct
+
+const root = createRoot(document.getElementById('root')); // Changed to getElementById
+
+
+
+
+
+const totalApple = 10
+let rightApplecount = 0
+let leftApplecount = totalApple - rightApplecount
 
 function AppleCounter() {
-  return (
-    <section>
-      <AppleBasket appleCount={10} basketName="basket 1"/>
 
-      <Button imageUrl={LeftArrow}  title="leftarrow" />
-      <Button imageUrl={RightArrow} title="rightarrow"/>
-      
-      <AppleBasket appleCount={0} basketName="basket 2" />
-    </section>
+  const leftClick = () => {
+    if (rightApplecount > 0) {
+      rightApplecount--
+    leftApplecount++
+    root.render( <AppleCounter/>);
+    }
+
+  }
+
+  const rightClick = () => {
+   if (leftApplecount > 0) {
+    rightApplecount++
+    leftApplecount--
+    root.render( <AppleCounter/>);
+   }
+    console.log(("left"),leftApplecount)
+    console.log(("right"),rightApplecount)
+  }
+
+  return (
+    <>
+      <section>
+        <AppleBasket appleCount={leftApplecount} basketName="basket 1" />
+
+        <Button imageUrl={LeftArrow} title="leftarrow" click={leftClick} />
+        <Button imageUrl={RightArrow} title="rightarrow" click={rightClick} />
+
+        <AppleBasket appleCount={rightApplecount} basketName="basket 2" />
+      </section>
+      {/* <p
+        style={{
+          textAlign: 'center',
+          margintop: "32px",
+
+        }}>                       
+        <button style={{    
+          width: "10vw",
+          height: "2vw",                   //eska to use nhi hua n  (button) , esko use kea the ham re-render krn time ep11
+                                           //last lapp
+        }} onClick={() => {
+          
+        }}>
+          Re-Render
+        </button>
+      </p> */}
+    </>
   )
 }
 export default AppleCounter
